fc172d7f51abf80063bf4fbc386b8e085b20a54a
ruhsuzrick <oumarsoukouna2008@yahoo.com>
2025-11-24 16:38:25 +0300

    Update AutoPot.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/AutoPot.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/AutoPot.java
@@ -1,12 +1,12 @@
-package dev.lvstrng.argon.module.modules.combat;
+package dev.lvstrng.grade.module.modules.combat;
 
-import dev.lvstrng.argon.event.events.TickListener;
-import dev.lvstrng.argon.module.Category;
-import dev.lvstrng.argon.module.Module;
-import dev.lvstrng.argon.module.setting.BooleanSetting;
-import dev.lvstrng.argon.module.setting.NumberSetting;
-import dev.lvstrng.argon.utils.EncryptedString;
-import dev.lvstrng.argon.utils.InventoryUtils;
+import dev.lvstrng.grade.event.events.TickListener;
+import dev.lvstrng.grade.module.Category;
+import dev.lvstrng.grade.module.Module;
+import dev.lvstrng.grade.module.setting.BooleanSetting;
+import dev.lvstrng.grade.module.setting.NumberSetting;
+import dev.lvstrng.grade.utils.EncryptedString;
+import dev.lvstrng.grade.utils.InventoryUtils;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.ActionResult;
