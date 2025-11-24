afc4c486e47928714c872309e9d23e64110ab6f5
ruhsuzrick <oumarsoukouna2008@yahoo.com>
2025-11-24 16:44:11 +0300

    Update TotemOffhand.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/TotemOffhand.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/TotemOffhand.java
@@ -1,12 +1,12 @@
-package dev.lvstrng.argon.module.modules.combat;
+package dev.lvstrng.grade.module.modules.combat;
 
-import dev.lvstrng.argon.event.events.TickListener;
-import dev.lvstrng.argon.module.Category;
-import dev.lvstrng.argon.module.Module;
-import dev.lvstrng.argon.module.setting.BooleanSetting;
-import dev.lvstrng.argon.module.setting.NumberSetting;
-import dev.lvstrng.argon.utils.EncryptedString;
-import dev.lvstrng.argon.utils.InventoryUtils;
+import dev.lvstrng.grade.event.events.TickListener;
+import dev.lvstrng.grade.module.Category;
+import dev.lvstrng.grade.module.Module;
+import dev.lvstrng.grade.module.setting.BooleanSetting;
+import dev.lvstrng.grade.module.setting.NumberSetting;
+import dev.lvstrng.grade.utils.EncryptedString;
+import dev.lvstrng.grade.utils.InventoryUtils;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.util.math.BlockPos;
