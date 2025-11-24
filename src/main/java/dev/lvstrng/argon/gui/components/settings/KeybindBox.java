e8e4ea8122f80996930685cb8edeb107b35074ce
ruhsuzrick <oumarsoukouna2008@yahoo.com>
2025-11-24 15:51:50 +0300

    Update KeybindBox.java

--- src/main/java/dev/lvstrng/argon/gui/components/settings/KeybindBox.java
+++ src/main/java/dev/lvstrng/argon/gui/components/settings/KeybindBox.java
@@ -1,11 +1,11 @@
-package dev.lvstrng.argon.gui.components.settings;
+package dev.lvstrng.grade.gui.components.settings;
 
-import dev.lvstrng.argon.gui.components.ModuleButton;
-import dev.lvstrng.argon.module.setting.KeybindSetting;
-import dev.lvstrng.argon.module.setting.Setting;
-import dev.lvstrng.argon.utils.ColorUtils;
-import dev.lvstrng.argon.utils.KeyUtils;
-import dev.lvstrng.argon.utils.TextRenderer;
+import dev.lvstrng.grade.gui.components.ModuleButton;
+import dev.lvstrng.grade.module.setting.KeybindSetting;
+import dev.lvstrng.grade.module.setting.Setting;
+import dev.lvstrng.grade.utils.ColorUtils;
+import dev.lvstrng.grade.utils.KeyUtils;
+import dev.lvstrng.grade.utils.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import org.lwjgl.glfw.GLFW;
 
